7e5d8116ec14949b1accf87bde35e61ed2ea2bb7
viegox1 <90939369+viegox1@users.noreply.github.com>
2021-09-17 20:17:58 -0300

    Add files via upload

---
+++ dotnet/Series registration APP/Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace Dio.Series.Classes
+{
+    public class EntidadeBase
+    {
+        
+    }
+}
---
+++ dotnet/Series registration APP/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dio.Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
